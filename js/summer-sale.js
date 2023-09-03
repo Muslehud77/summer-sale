d4fd6a05246823867b8e8a9086390b262c28a45c
Muslehud77 <fardinmohit@gmail.com>
2023-09-04 02:07:41 +0800

    updradedwithLocalstorage

--- js/summer-sale.js
+++ js/summer-sale.js
@@ -23,15 +23,35 @@ function quantityChanged(data){
         quantity.value = 1;
     }
     updateTotal();
+    const itemName = data.parentNode.parentNode.childNodes[1].childNodes[3].innerText
+    for(let i = 0; i < localStorage.length; i++){
+        // console.log(localStorage.getItem(localStorage.key(i)))
+        const localStorageItem = JSON.parse(localStorage.getItem(localStorage.key(i)))
+        if(localStorageItem.name === itemName){
+            localStorageItem['quantity'] = quantity.value
+            localStorage.setItem(itemName, JSON.stringify(localStorageItem))
+            console.log(localStorageItem)
+        }
+    }
+
+
+
+    // console.log(data.parentNode.parentNode.childNodes[1].childNodes[3].innerText)
+
+    
 }
+
+let discount = false;
 
 function discountEnable(){
 const discountButton = document.getElementById("discount");
 
 if (parseInt(totalPriceWithoutDiscount.innerText) >= 200) {
+   
   discountButton.disabled = false;
   discountInput.disabled = false;
 }else{
+    
    discountButton.disabled = true; 
    discountInput.disabled = true;
    discountInput.value = "";
@@ -52,76 +72,121 @@ function coupon(data){
 function applyButton(){
    
    if(discountInput.value === 'SELL200'){
+        discount = true;
         discountAmount.innerText = ((20/100)*parseFloat(totalPriceWithoutDiscount.innerText)).toFixed(2);
         updateTotal();
         discountInput.value = '';
+       
    }
+    
 }
 
 
 
 function removeButton(data){
+    const itemName = data.parentNode.parentNode.childNodes[1].childNodes[3].innerText
+    localStorage.removeItem(itemName)
     data.parentNode.parentNode.parentNode.remove();
     updateTotal();
 }
 
-
-function itemClicked(data){
-    const cartList = document.getElementById("items-list");
-    const productImg = data.children[0].childNodes[1].children[0];
-    const productName = data.childNodes[3].childNodes[3].innerText
-    const productPrice = data.childNodes[3].childNodes[5].innerText.split(' ')[0]
-    const itemList = document.getElementById('items-list');
-    const div = document.createElement('div')
-    div.innerHTML = `<div class="flex justify-between items-center gap-3 w-80">
-                            <div class="flex items-center gap-1">
-    <div class="bg-gray-300 p-2 rounded-xl w-12 h-12"><img class="" src="${productImg.src}" alt="">
-                            </div>
-                            <p class="text-base w-10">${productName}</p>
-</div>
-                            <p class="text-base font-semibold w-10"><span class="price-item">${productPrice}</span> TK</p>
-                            <div class="flex items-center gap-2">
-                                <input onchange="quantityChanged(this)" value="1" type="number" min="1"
-                                    class="input item-quantity input-bordered input-sm w-12 p-1" />
-                                <button onclick="removeButton(this)"
-                                    class="remove bg-red-500 rounded-full text-white hover:bg-red-600 p-1 px-2"><i
-                                        class="fa-solid fa-xmark"></i></button>
-                            </div>
-                        </div>`;
-
-    for (const list of cartList.children) {
-        const itemInList = list.childNodes[0].children[0].children[1].innerText;
-        const itemToBeAdded = div.childNodes[0].children[0].children[1].innerText;
-        if (itemToBeAdded == itemInList) {
-          return 0;
-        } else {
-          div.innerHTML = `<div class="flex justify-between items-center gap-3 w-80">
-                            <div class="flex items-center gap-1">
-    <div class="bg-gray-300 p-2 rounded-xl w-12 h-12"><img class="" src="${productImg.src}" alt="">
-                            </div>
-                            <p class="text-base w-10">${productName}</p>
-</div>
-                            <p class="text-base font-semibold w-10"><span class="price-item">${productPrice}</span> TK</p>
-                            <div class="flex items-center gap-2">
-                                <input onchange="quantityChanged(this)" value="1" type="number" min="1"
-                                    class="input item-quantity input-bordered input-sm w-12 p-1" />
-                                <button onclick="removeButton(this)"
-                                    class="remove bg-red-500 rounded-full text-white hover:bg-red-600 p-1 px-2"><i
-                                        class="fa-solid fa-xmark"></i></button>
-                            </div>
-                        </div>`;
-        }
-    } 
-    itemList.appendChild(div);
-    updateTotal();
+const storedCart = () =>{
+    let cart = {};
+    const storedCart = localStorage.getItem("cart");
+    
+    if(storedCart){
+        cart = JSON.parse(storedCart);
+    }
+    // console.log(cart)
+    return cart;
 }
 
 
+
+
+
+const addLocalStore = (productImg, productName, productPrice) => {
+    const cart = storedCart();
+   
+//    console.log(quantity)
+   cart['name'] = productName;
+   cart['image'] = productImg;
+   cart['price'] = productPrice;
+   //    console.log(cart)
+   
+   localStorage.setItem(productName, JSON.stringify(cart));
+   
+    
+}
+
+
+
+
+
+
+function itemClicked(data){
+    // const itemList = document.getElementById("items-list");
+    const productImg = data.children[0].childNodes[1].children[0].src;
+    const productName = data.childNodes[3].childNodes[3].innerText
+    const productPrice = data.childNodes[3].childNodes[5].innerText.split(' ')[0]
+   updateCart(productImg, productName, productPrice);
+   addLocalStore(productImg, productName, productPrice);
+}
+
+const updateCart = (productImg, productName, productPrice,quantity=1) => {
+  const itemList = document.getElementById("items-list");
+  const div = document.createElement("div");
+  div.innerHTML = `<div class="flex justify-between items-center gap-3 w-80">
+                            <div class="flex items-center gap-1">
+    <div class="bg-gray-300 p-2 rounded-xl w-12 h-12"><img class="" src="${productImg}" alt="">
+                            </div>
+                            <p class="text-base w-10">${productName}</p>
+</div>
+                            <p class="text-base font-semibold w-10"><span class="price-item">${productPrice}</span> TK</p>
+                            <div class="flex items-center gap-2">
+                                <input onchange="quantityChanged(this)" value="${quantity||1}" type="number" min="1"
+                                    class="input item-quantity input-bordered input-sm w-12 p-1" />
+                                <button onclick="removeButton(this)"
+                                    class="remove bg-red-500 rounded-full text-white hover:bg-red-600 p-1 px-2"><i
+                                        class="fa-solid fa-xmark"></i></button>
+                            </div>
+                        </div>`;
+  for (const list of itemList.children) {
+    const itemInList = list.childNodes[0].children[0].children[1].innerText;
+    const itemToBeAdded = div.childNodes[0].children[0].children[1].innerText;
+    if (itemToBeAdded == itemInList) {
+      return 0;
+    } else {
+      div.innerHTML = `<div class="flex justify-between items-center gap-3 w-80">
+                            <div class="flex items-center gap-1">
+    <div class="bg-gray-300 p-2 rounded-xl w-12 h-12"><img class="" src="${productImg}" alt="">
+                            </div>
+                            <p class="text-base w-10">${productName}</p>
+</div>
+                            <p class="text-base font-semibold w-10"><span class="price-item">${productPrice}</span> TK</p>
+                            <div class="flex items-center gap-2">
+                                <input onchange="quantityChanged(this)" value="${
+                                  quantity || 1
+                                }" type="number" min="1"
+                                    class="input item-quantity input-bordered input-sm w-12 p-1" />
+                                <button onclick="removeButton(this)"
+                                    class="remove bg-red-500 rounded-full text-white hover:bg-red-600 p-1 px-2"><i
+                                        class="fa-solid fa-xmark"></i></button>
+                            </div>
+                        </div>`;
+    }
+  }
+  itemList.appendChild(div);
+  updateTotal();
+  
+};
+
+
 function updateTotal(){
-    const cartList = document.getElementById("items-list");
+    const itemList = document.getElementById("items-list");
     
     let price = 0;
-    for(const list of cartList.children){
+    for(const list of itemList.children){
         const priceItem = list.children[0].children[1].children[0].innerText;
         const quantity = list.children[0].children[2].children[0].value;
        
@@ -144,6 +209,7 @@ function gohomeButton(){
     totalPriceWithDiscount.innerText = '00'
     discountAmount.innerText = '00'
     updateTotal();
+    localStorage.clear();
 }
 function discountUpdate(){
     if(parseFloat(discountAmount.innerText)>0){
@@ -151,3 +217,27 @@ function discountUpdate(){
     }
     
 }
+
+
+const displayStoredCart = () => {
+  for (let i = 0; i < localStorage.length; i++) {
+    const storedItem = localStorage.getItem(localStorage.key(i));
+    const getItem = JSON.parse(storedItem);
+
+    const name = getItem.name;
+    const image = getItem.image;
+    const price = getItem.price;
+    const quantity = getItem.quantity;
+    if(name){
+     updateCart(image, name, price, quantity);
+    }
+    
+    // console.log(getItem.name);
+    // console.log(getItem.image);
+    // console.log(getItem.price);
+  }
+//   const discountStatus = JSON.parse(localStorage.getItem(discount))
+  
+};
+
+displayStoredCart();
